e40020798470f915f5a78f5b7e8ad6e19a4c2b7c
Paul Browne <paulbrowne2@gmail.com>
2021-03-22 06:40:07 +0000

    Set up heroku

--- music_bits/settings.py
+++ music_bits/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'dh7%^vazx@^97y5m*)afl$#dk586$f(!tvbvc7r%5!fesvwa2&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pbtrad-music-bits.herokuapp.com' 'localhost']
 
 
 # Application definition
@@ -117,13 +117,17 @@ WSGI_APPLICATION = 'music_bits.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
