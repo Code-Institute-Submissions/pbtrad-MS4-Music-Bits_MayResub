37244429956794847c83b51958bbdbe030561eb6
Paul Browne <paulbrowne2@gmail.com>
2021-03-22 13:50:18 +0000

    fixed indent in checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -38,7 +38,7 @@ def checkout(request):
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     if request.method == 'POST':
-        bag = request.session.get('cart', {})
+        cart = request.session.get('cart', {})
 
         form_data = {
             'full_name': request.POST['full_name'],
