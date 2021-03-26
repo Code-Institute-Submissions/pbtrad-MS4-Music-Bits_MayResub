8e2ad6e9efebcea26c80d1f2611e97a4f8b5c89b
Paul Browne <paulbrowne2@gmail.com>
2021-03-26 14:06:47 +0000

    Added some styling to toasts

--- music_bits/urls.py
+++ music_bits/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-    path('coupon_apply/', include('coupons.urls')),
+    path('coupons/', include('coupons.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
