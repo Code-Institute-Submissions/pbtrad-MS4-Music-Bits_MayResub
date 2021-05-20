7cddc3a967e4667c6ad1a4d9e6d03d75759bc134
Paul Browne <paulbrowne2@gmail.com>
2021-05-20 06:18:45 +0100

    Added validator to rating field in Product model so only positive decimals may be entered

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator
+from decimal import Decimal
 
 # Create your models here.
 
@@ -30,7 +31,7 @@ class Product(models.Model):
     string_gauge = models.BooleanField(default=False, null=True, blank=True)
     price = models.PositiveIntegerField()
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
-                                 blank=True)
+                                 blank=True, validators=[MinValueValidator(Decimal('0.01'))])
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
